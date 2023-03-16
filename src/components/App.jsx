bfd4db6f0069a5e64e2b3b9885fd58aa0a153ea4
Yevhenii <boiko.yevgenii@gmail.com>
2023-03-16 23:42:07 +0200

    adds links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        backgroundColor: '#3f51b5',
       }}
     >
-      React homework template
+      <ul
+        style={{
+          display: 'flex',
+          listStyle: 'none',
+          gap: '30px',
+          fontSize: '26px',
+          justifyContent: 'center',
+          paddingLeft: '0px',
+          paddingTop: '30px',
+          paddingBottom: '30px',
+          margin: '0px',
+        }}
+      >
+        <li>
+          <Link to="/" style={{ color: 'white' }}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/movies" style={{ color: 'white' }}>
+            Movies
+          </Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route />
+        <Route />
+      </Routes>
     </div>
   );
 };
