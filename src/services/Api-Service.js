4aa8960695cd365a516c4dd3aebdb5f1f014b6f4
Yevhenii <boiko.yevgenii@gmail.com>
2023-03-18 17:05:47 +0200

    updates api-service

--- src/services/Api-Service.js
+++ src/services/Api-Service.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-getMovies() {
-    const BASE_URL = 'https://api.themoviedb.org/3/';
-    const API_KEY = '12a1c9f7c08bf08c5c3c451723bbd88a';
-    return axios.get(`${BASE_URL}${API_KEY}`).then(promise => {
-        return promise.results;
-    });
-}
+// getMovies() {
+//     const BASE_URL = 'https://api.themoviedb.org/3/';
+//     const API_KEY = '12a1c9f7c08bf08c5c3c451723bbd88a';
+//     return axios.get(`${BASE_URL}${API_KEY}`).then(promise => {
+//         return promise.results;
+//     });
+// }
